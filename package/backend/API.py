d922c933d41ab8ba5b8720b24256077786d065c0
Areeba Aziz <aziza11@mcmaster.ca>
2020-03-17 02:50:42 -0400

    committed by Areeba

--- package/backend/API.py
+++ package/backend/API.py
@@ -1,2 +1,12 @@
 class API:
-    pass 
+    
+    def __init__(self):
+        self.user = None
+
+    def login(self, username, password):
+        # TODO: code for logging in user
+        return True 
+
+    def signup(self, username, pass1, pass2):
+        # TODO: code for signing in user
+        return True
--- package/frontend/mainwindow.py
+++ package/frontend/mainwindow.py
@@ -9,17 +9,19 @@ from package.backend import API
 PACKAGE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 UI_PATH = os.path.join(PACKAGE_DIR, "ui")
 
-qt_creator_file = os.path.join(UI_PATH, "mainwindow.ui")
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator_file)
+welcome_window_ui = os.path.join(UI_PATH, "mainwindow.ui")
+welcome_window, QtBaseClass = uic.loadUiType(welcome_window_ui)
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class MainWindow(QtWidgets.QMainWindow, welcome_window):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
+        welcome_window.__init__(self)
         self.setupUi(self)
 
         self.api = API()
+        self.register_elements()
 
+    def register_elements(self):
         self.btn_login = self.findChild(QtWidgets.QPushButton, 'btn_login')
         self.btn_login.clicked.connect(self.clicked_btn_login)
         self.btn_signup = self.findChild(QtWidgets.QPushButton, 'btn_signup')
@@ -35,6 +37,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         username = self.le_login_username.text()
         password = self.le_login_pass.text()
         logging.debug("Username: {}, Password: {}".format(username, password))
+        res = self.api.login(username, password)
+        if res: 
+            logging.debug("Login successful.")
+            self.switch_to_dashboard()
+        else: 
+            logging.debug("Login failed.")
 
     def clicked_btn_signup(self):
         logging.debug("Signup button clicked.")
@@ -42,3 +50,13 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         pass1 = self.le_signup_pass1.text()
         pass2 = self.le_signup_pass2.text()
         logging.debug("Username: {}, Pass1: {}, Pass2: {}".format(username, pass1, pass2))
+        res = self.api.signup(username, pass1, pass2)
+        if res: 
+            logging.debug("Signup successful.")
+            self.switch_to_dashboard()
+        else: 
+            logging.debug("Signup failed.")
+
+    def switch_to_dashboard(self):
+        # TODO: code to switch windows to dashboard 
+        pass 
